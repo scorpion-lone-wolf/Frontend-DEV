5726746e43c8889df719dcab34ca2c980dff5c4a
Pursotam <rpursotam99@gmail.com>
2024-06-15 17:08:25 +0530

    completed homepage for desktop application

--- projects/css-projects/multipage-education-school-website/src/main.js
+++ projects/css-projects/multipage-education-school-website/src/main.js
@@ -5,3 +5,22 @@ window.addEventListener('scroll', function () {
     // * If it has "window-scroll" class then it remove, else add
     navElement[0].classList.toggle('window-scroll', window.scrollY > 0);
 })
+
+// * add event listener to faq article elements
+const faqElements = document.querySelectorAll('.faq');
+
+// * Attach a click event listener to each element
+faqElements.forEach(element => {
+    element.addEventListener('click', (event) => {
+        element.classList.toggle('open');
+        // <i class="fa-solid fa-minus"></i>
+        // *change the icon
+        const icon = element.querySelector('.faq__icon i');
+        if (icon.className === 'fa-solid fa-minus') {
+            icon.className = 'fa-solid fa-plus';
+        } else {
+            icon.className = 'fa-solid fa-minus';
+        }
+
+    });
+});
