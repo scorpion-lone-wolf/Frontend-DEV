c5017c1bf0e4b7388d3c730d061b72dd430bd881
pusotam <rpusotam99@gmail.com>
2024-07-19 12:15:43 +0530

    optimised performance in tic tac toe

--- TECH-PROJECTS/JS/Tic-Tac-Toe/script.js
+++ TECH-PROJECTS/JS/Tic-Tac-Toe/script.js
@@ -1,58 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const boxes = document.querySelectorAll('.items')
     const TURNS = ['<i class="fa-solid fa-xmark"></i>', '<i class="fa-solid fa-o"></i>']
     let currentTurn = 0
+    const currentTurnElemnt = document.querySelector('.current-turn')
+    currentTurnElemnt.textContent = currentTurn == 0 ? "X Turn" : "O Turn"
     const grid = new Array(3).fill(null).map(() => new Array(3).fill(""))
     let isGameOn = true
-    for (const ele of boxes) {
-        ele.addEventListener("click", function (e) {
-            let wonBy = ""
-            let moveDone = false
-            if (isGameOn) {
-                const elementIndex = ele.classList[1].split('-')[1]
-                if (elementIndex <= 3) {
-                    // first row
-                    if (grid[0][(elementIndex - 1) % 3] === '') {
-                        ele.innerHTML = TURNS[Number(currentTurn)]
-                        grid[0][(elementIndex - 1) % 3] = TURNS[Number(currentTurn)]
-                        ele.style.cursor = "not-allowed"
-                        ele.style.fontSize = "2.3rem"
-                        ele.style.textAlign = "center"
-                        moveDone = true
-                    }
-                } else if (elementIndex <= 6) {
-                    // second row
-                    if (grid[1][(elementIndex - 1) % 3] === '') {
-                        ele.innerHTML = TURNS[Number(currentTurn)]
-                        grid[1][(elementIndex - 1) % 3] = TURNS[Number(currentTurn)]
-                        ele.style.cursor = "not-allowed"
-                        ele.style.fontSize = "2.3rem"
-                        ele.style.textAlign = "center"
-                        moveDone = true
-                    }
-                } else {
-                    // third row
-                    if (grid[2][(elementIndex - 1) % 3] === '') {
-                        ele.innerHTML = TURNS[Number(currentTurn)]
-                        grid[2][(elementIndex - 1) % 3] = TURNS[Number(currentTurn)]
-                        ele.style.cursor = "not-allowed"
-                        ele.style.fontSize = "2.3rem"
-                        ele.style.textAlign = "center"
-                        moveDone = true
-                    }
-                }
-                if (moveDone) {
-                    currentTurn = !currentTurn
-                    const result = checkForGameOutcome(grid)
-                    isGameOn = result[0]
-                    wonBy = result[1]
-                    if (!isGameOn) {
-                        updateResult(wonBy)
-                    }
+    const parent = document.querySelector('.container')
+
+    parent.addEventListener('click', (event) => {
+        const ele = event.target
+        if (isGameOn) {
+            const elementIndex = ele.classList[1].split('-')[1]
+            const rowIndex = Math.floor((elementIndex - 1) / 3)
+            const colIndex = Math.floor((elementIndex - 1) % 3)
+
+            if (grid[rowIndex][colIndex] === '') {
+                ele.innerHTML = TURNS[Number(currentTurn)]
+                grid[rowIndex][colIndex] = TURNS[Number(currentTurn)]
+                ele.style.cursor = "not-allowed"
+                ele.style.fontSize = "2.3rem"
+                ele.style.textAlign = "center"
+
+                currentTurn = !currentTurn
+                currentTurnElemnt.textContent = currentTurn == 0 ? "X Turn" : "O Turn"
+
+                console.log(grid)
+                const [gameOn, winner] = checkForGameOutcome(grid)
+                isGameOn = gameOn
+                if (!isGameOn) {
+                    updateResult(winner)
                 }
             }
-        })
-    }
+
+        }
+    })
+
     function updateResult(wonBy) {
         if (wonBy)
             document.getElementsByClassName('result')[0].textContent = `🎇 Player ${TURNS[0] == wonBy ? "X" : "O"} Won The Match 🎇`
@@ -70,28 +52,15 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function checkForGameOutcome(grid) {
-
-        // *check for draw
-        if (isDraw(grid)) return [false, null]
-
-        // *check for all rows
-
+        // * check for all rows & columns
         for (let i = 0; i < 3; ++i) {
-            for (let j = 0; j < 3; j++) {
-                if (grid[0][0] != "" && grid[0][0] === grid[0][1] && grid[0][1] === grid[0][2]) {
-                    return [false, grid[0][0]]
-                }
+
+            if (grid[i][0] != "" && grid[i][0] === grid[i][1] && grid[i][1] === grid[i][2]) {
+                return [false, grid[0][0]]
             }
-        }
-        // *check for all columns
-        if (grid[0][0] != "" && grid[0][0] === grid[1][0] && grid[1][0] === grid[2][0]) {
-            return [false, grid[0][0]]
-        }
-        if (grid[0][1] != "" && grid[0][1] === grid[1][1] && grid[1][1] === grid[2][1]) {
-            return [false, grid[0][1]]
-        }
-        if (grid[0][2] != "" && grid[0][2] === grid[1][2] && grid[1][2] === grid[2][2]) {
-            return [false, grid[0][2]]
+            if (grid[0][i] != "" && grid[0][i] === grid[1][i] && grid[1][i] === grid[2][i]) {
+                return [false, grid[0][0]]
+            }
         }
 
         // *check for diagonals
@@ -101,6 +70,10 @@ document.addEventListener("DOMContentLoaded", function () {
         if (grid[0][2] != "" && grid[0][2] === grid[1][1] && grid[1][1] === grid[2][0]) {
             return [false, grid[0][2]]
         }
+
+        // *check for draw
+        if (isDraw(grid)) return [false, null]
+
         // *game is still on
         return [true, null]
     }
